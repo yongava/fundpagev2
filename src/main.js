19a27cdc95edfb5916a331f73ee89d4b5f4c6d2f
pci-qs <cpak@qompass-solutions.com>
2020-12-03 16:05:08 +0300

    changed url for desktop

--- src/main.js
+++ src/main.js
@@ -7,29 +7,28 @@ import VueAxios from 'vue-axios'
 import VueTouch from 'vue-touch';
 //import bootstrapvue from 'bootstrap-vue'
 
-if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-  
-  // console.log('Mobile')
-  const BASE_URL = 'https://lineoa-api.ava.fund';
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
 
-  VueTouch.config.swipe = {
-    direction: 'horizontal',
-    threshold: 200
-  };
+    // console.log('Mobile')
+    const BASE_URL = 'https://lineoa-api.ava.fund';
 
-  Vue.config.productionTip = false
-  Vue.use(VModal);
-  Vue.use(VueTouch);
-  Vue.use(VueAxios, axios.create({ baseURL: BASE_URL }));
+    VueTouch.config.swipe = {
+        direction: 'horizontal',
+        threshold: 200
+    };
 
-  new Vue({
-    render: h => h(App),
-  }).$mount('#app')
- }
- else
- {
-  //  console.log('Win')
-   window.location.href = 'https://www.scbam.com';
- }
+    Vue.config.productionTip = false
+    Vue.use(VModal);
+    Vue.use(VueTouch);
+    Vue.use(VueAxios, axios.create({ baseURL: BASE_URL }));
 
-
+    new Vue({
+        render: h => h(App),
+    }).$mount('#app')
+} else {
+    const urlParams = new URLSearchParams(window.location.search);
+    const fundCode = urlParams.get("FundCode");
+    //const userID = urlParams.get("UserID");
+    //  console.log('Win')
+    window.location.href = `https://www.scbam.com/th/fund/morningstar/fund-information/${fundCode}`;
+}
