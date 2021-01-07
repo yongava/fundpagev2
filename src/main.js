99a188de99a8b7429ef192b983725e4edca7c288
YongAVA <raksasakul@ava.fund>
2021-01-07 17:46:43 +0700

    user agent

--- src/main.js
+++ src/main.js
@@ -20,11 +20,11 @@ const fundCode = urlParams.get("FundCode");
 
 
 if (/Windows|Ububtu/i.test(navigator.userAgent)) {
-    console.log('windows  ',/Windows|Ububtu/i.test(navigator.userAgent))
+    console.log('windows  ',/Windows|Ububtu/i.test(navigator.userAgent),navigator.userAgent)
     window.location.href = `https://www.scbam.com/th/fund/morningstar/fund-information/${fundCode}`;
 } else {
     //Mozilla/5.0 (Linux; Android 8.1.0; SM-N960U Build/M1AJQ) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.91 Mobile Safari/537.36
-    console.log('Mobile  ',/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))
+    console.log('Mobile  ',/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent),navigator.userAgent)
     const BASE_URL = 'https://lineoa-api.ava.fund';
 
     VueTouch.config.swipe = {
